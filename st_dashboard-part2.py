19fad6ef4f2ac43aa79f610c011dc82d33941075
Raghu Kummari <raghu.kummari@outlook.com>
2024-12-11 20:19:56 +0100

    Updated Streamlit

--- st_dashboard-part2.py
+++ st_dashboard-part2.py
@@ -41,8 +41,8 @@ if page == "Intro page":
     st.markdown("- Interactive map with aggregated bike trips")
     st.markdown("- Recommendations")
     st.markdown("The dropdown menu on the left 'Aspect Selector' will take you to the different aspects of the analysis our team looked at.")
-
-    myImage = Image.open("/users/gideon/DivvyBikes_2022/DivvyImage.jpeg") 
+    
+    myImage = Image.open("/users/gideon/DivvyBikes_2022/Intropageimage.jpg") 
     st.image(myImage)
 
 
@@ -51,22 +51,19 @@ if page == "Intro page":
 elif page == 'Weather component and bike usage':
 
     fig_2 = make_subplots(specs = [[{"secondary_y": True}]])
-
     fig_2.add_trace(
     go.Scatter(x = df['date'], y = df['bike_rides_daily_x'], name = 'Daily bike rides', marker={'color': df['bike_rides_daily_x'],'color': 'blue'}),
-    secondary_y = False
-    )
-
-    fig_2.add_trace(
-    go.Scatter(x=df['date'], y = df['avgTemp'], name = 'Daily temperature', marker={'color': df['avgTemp'],'color': 'red'}),
-    secondary_y=True
-    )
-
+        secondary_y = False
+        )
+    
+    fig_2.add_trace(go.Scatter(x=df['date'], y = df['avgTemp'], name = 'Daily temperature', marker={'color': df['avgTemp'],'color': 'red'}),
+        secondary_y=True
+        )
     fig_2.update_layout(
-    title = 'Daily bike trips and temperatures in 2022',
-    height = 400
-    )
-
+        title = 'Daily bike trips and temperatures in 2022',
+        height = 400
+        )
+    
     st.plotly_chart(fig_2, use_container_width=True)
     st.markdown("There is an obvious correlation between the rise and drop of temperatures and their relationship with the frequency of bike trips taken daily. As temperatures plunge, so does bike usage. This insight indicates that the shortage problem may be prevalent merely in the warmer months, approximately from May to October.")
 
@@ -112,27 +109,27 @@ elif page == 'Most popular stations':
     # width = 900, height = 600sxs
     # )
     # st.plotly_chart(fig, use_container_width=True)
-    st.markdown("From the bar chart it is clear that there are some start stations that are more popular than others - in the top 3 we can see Streeter Drive/Grand Avenue, Canal Street/Adams Street as well as Clinton Street/Madison Street. There is a big jump between the highest and lowest bars of the plot indicating some clear preferences for the leading stations. This is a finding that we could cross reference with the interactive map that you can access through the side bar select box.")
+    st.markdown("From the bar chart it is clear that there are some start stations that are more popular than others - in the top 3 we can see start_station_name Grove St PATH, South Waterfront Walkway - Sinatra Dr & 1 St as well as Hoboken Terminal - Hudson St & Hudson Pl. There is a big jump between the highest and lowest bars of the plot indicating some clear preferences for the leading stations. This is a finding that we could cross reference with the interactive map that you can access through the side bar select box.")
 
-elif page =='Interactive map with aggregated bike trips': 
+# elif page =='Interactive map with aggregated bike trips': 
 
-    ### Create the map ###
+#     ### Create the map ###
 
-    st.write("Interactive map showing aggregated bike trips over New York")
+#     st.write("Interactive map showing aggregated bike trips over New York")
 
-  ##  path_to_html = "/users/gideon/DivvyBikes_2022/kepler_map.html" 
+#     path_to_html = "/users/gideon/DivvyBikes_2022/kepler_map.html" 
 
-    # Read file and keep in variable
-    with open(path_to_html,'r') as f: 
-        html_data = f.read()
+#     # Read file and keep in variable
+#     with open(path_to_html,'r') as f: 
+#         html_data = f.read()
 
-    ## Show in webpage
-    st.header("Aggregated Bike Trips in New York")
-    st.components.v1.html(html_data,height=1000)
-    st.markdown("#### Using the filter on the left hand side of the map we can check whether the most popular start stations also appear in the most popular trips.")
-    st.markdown("The most popular start stations are:")
-    st.markdown("Streeter Drive/Grand Avenue, Canal Street/Adams Street as well as Clinton Street/Madison Street. While having the aggregated bike trips filter enabled, we can see that even though Clinton Street/Madison Street is a popular start stations, it doesn't account for the most commonly taken trips.")
-    st.markdown("The most common routes (>2,000) are between Theater on the Lake, Streeter Dr/Grand Avenue, Millenium Park, Columbus Dr/Randolph Street, Shedd Aquarium, Michigan Avenue/Oak Street, Canal Street/Adams Street, which are predominantly located along the water.")
+#     ## Show in webpage
+#     st.header("Aggregated Bike Trips in New York")
+#     st.components.v1.html(html_data,height=1000)
+#     st.markdown("#### Using the filter on the left hand side of the map we can check whether the most popular start stations also appear in the most popular trips.")
+#     st.markdown("The most popular start stations are:")
+#     st.markdown("Grove St PATH, South Waterfront Walkway - Sinatra Dr & 1 St as well as Hoboken Terminal - Hudson St & Hudson Pl.")
+#     st.markdown("The most common routes (>2,000) are between Washington St, Sip Ave, Madison St & 1 St, Marin Light Rail, Newport Pkwy, which are predominantly located along the water.")
 
 else:
     
@@ -140,5 +137,5 @@ else:
     bikes = Image.open("/users/gideon/DivvyBikes_2022/recommendation.png")  #source: Midjourney
     st.image(bikes)
     st.markdown("### Our analysis has shown that Divvy Bikes should focus on the following objectives moving forward:")
-    st.markdown("- Add more stations to the locations around the water line, such as heater on the Lake, Streeter Dr/Grand Avenue, Millenium Park, Columbus Dr/Randolph Street, Shedd Aquarium, Michigan Avenue/Oak Street, Canal Street/Adams Street")
+    st.markdown("- Add more stations to the locations around the water line, such as Washington St, Sip Ave, Madison St & 1 St, Marin Light Rail, Newport Pkwy")
     st.markdown("- Ensure that bikes are fully stocked in all these stations during the warmer months in order to meet the higher demand, but provide a lower supply in winter and late autumn to reduce logistics costs")
